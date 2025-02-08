6ac5735d62e86a9926df7122832b36e65a1e9ebc
Igor Minar <iminar@cloudflare.com>
2025-02-08 12:22:20 -0800

    simplification

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ import { HTMLRewriter } from 'htmlrewriter';
 export async function repro() {
 	console.log('reprooooooo\n\n\n');
 
-	const webReadableInput2 = new ReadableStream({
-		start(controller) {
-			controller.enqueue('<html><meta charset="UTF-8"><body></body>');
-			controller.close();
-		},
-	}).pipeThrough(new TextEncoderStream());
-
 	const rewrittenResponse = new HTMLRewriter()
 		.on('body', {
 			element(element) {
@@ -17,13 +10,9 @@ export async function repro() {
 				element.append('<p><p>👉<p></p></p></p>', { html: true });
 			},
 		})
-		.transform(new Response(webReadableInput2));
+		.transform(new Response('<html><meta charset="UTF-8"><body></body>'));
 
 	const text = await rewrittenResponse.text();
   console.log('xxx', text);
-	// const rewrittenBody = rewrittenResponse.body;
-	// const nodeReadableOuput = stream.Readable.fromWeb(rewrittenBody as streamWeb.ReadableStream);
-
-	// return nodeReadableOuput;
   return text;
 }
--- worker.js
+++ worker.js
@@ -3,6 +3,5 @@ import { repro } from './index.js';
 export default {
   fetch: async () => {
     return new Response(await repro());
-    //return new Response('done');
   }
 }
